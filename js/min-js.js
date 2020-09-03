2f536d3f67c1f33d567abdd22e2cc8b0f202877a
UA_Girl <n_kholodkova@ukr.net>
2020-09-03 21:21:26 +0300

    Index page

---
+++ js/min-js.js
@@ -0,0 +1,16 @@
+let backref_open = document.getElementById("backref_open");
+let modal_form  = document.getElementsByClassName("modal-backref")[0];
+let close_popup = document.getElementById("modal-backref-close");
+
+
+backref_open.addEventListener("click", function (event) {
+    let overlay = document.getElementsByClassName("overlay")[0];
+    modal_form.style.transform = "scale(1)";
+    overlay.style.transform = "scale(1)";
+});
+
+close_popup.addEventListener("click", function (event) {
+    let overlay = document.getElementsByClassName("overlay")[0];
+    modal_form.style.transform = "scale(0)";
+    overlay.style.transform = "scale(0)";
+});
