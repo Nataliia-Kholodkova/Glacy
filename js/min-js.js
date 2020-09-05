930c1e6453f19a0b799d0304d44d2964b39e3d54
UA_Girl <n_kholodkova@ukr.net>
2020-09-05 21:50:29 +0300

    Added focus

--- js/min-js.js
+++ js/min-js.js
@@ -9,6 +9,12 @@ backref_open.addEventListener("click", function (event) {
     overlay.style.transform = "scale(1)";
 });
 
+backref_open.addEventListener("focus", function (event) {
+    let overlay = document.getElementsByClassName("overlay")[0];
+    modal_form.style.transform = "scale(1)";
+    overlay.style.transform = "scale(1)";
+});
+
 close_popup.addEventListener("click", function (event) {
     let overlay = document.getElementsByClassName("overlay")[0];
     modal_form.style.transform = "scale(0)";
